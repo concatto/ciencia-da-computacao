683184f464c93b4ff67c16c2bac5f04eb6cc70a8
Fernando Concatto <fernandoconcatto@gmail.com>
2016-05-04 03:27:38 -0300

    ieee754

---
+++ Arquitetura e Organização de Computadores/ieee754.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+
+std::string calculateMantissa(unsigned value) {
+
+}
+
+int main() {
+    std::string input = "10,5";
+    int a = std::atoi(input.substr(0, input.find_first_of(',')).c_str());
+    int b = std::atoi(input.substr(input.find_first_of(',') + 1).c_str());
+    std::cout << a << " ; " << b << "\n";
+}
