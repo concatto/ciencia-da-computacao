1979c129162dff21eaa793c50407027e0a5958b4
Fernando Concatto <fernandoconcatto@gmail.com>
2016-06-13 18:13:32 -0300

    Pasta do minicurso adicionada

---
+++ Minicurso/index.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var SerialPort = require("serialport").SerialPort;
+var http = require("http");
+var io = require("socket.io");
+
+var app = express();
+var server = http.Server(app);
+var socketServer = io(server);
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+server.listen(4000);
