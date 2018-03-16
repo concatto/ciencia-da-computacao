fa98cbf2d4d54b48e0bc294a0eb9a3bdf069cb97
Fernando Concatto <fernandoconcatto@gmail.com>
2018-03-16 20:46:39 -0300

    Update git-setup.ps1

--- git-setup.ps1
+++ git-setup.ps1
@@ -8,3 +8,8 @@ If (!(Get-ChildItem -Directory $env:HOMEPATH).Name.Contains($directory)) {
   Start-Process "./$filename" -Wait
 }
 $env:PATH = "$env:PATH;C:$env:HOMEPATH/$directory/bin"
+
+
+
+# Minified
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;$directory = "PortableGit";$filename = "PortableGit-2.16.2-64-bit.7z.exe";$path = ".";If (!(Get-ChildItem -Directory $env:HOMEPATH).Name.Contains($directory)) {  $url = "https://github.com/git-for-windows/git/releases/download/v2.16.2.windows.1/$filename"; Invoke-WebRequest $url -OutFile "$path/$filename";  Start-Process "./$filename" -Wait;}$env:PATH = "$env:PATH;C:$env:HOMEPATH/$directory/bin";
