57d86922deeb24636b4cdb639d978c23cc705b9b
Fernando Concatto <fernandoconcatto@gmail.com>
2018-05-28 23:36:43 -0300

    Geração de gráficos quase pronta, só aguardar dados

--- Inteligência Artificial/NeuralNetwork/analysis.py
+++ Inteligência Artificial/NeuralNetwork/analysis.py
@@ -4,9 +4,28 @@ import numpy as np
 
 path = "/home/concatto/Downloads/GoodResultsNN/4/experiments_portion0.csv"
 df = pd.read_csv(path)
+df = df.loc[df['hidden_activation'] == 'sigmoid']
+df = df.loc[df['output_activation'] == 'sigmoid']
 
-filtered = df.loc[df['learning_rate'] == 0.001]
-acc = filtered['best_validation_acc']
+def generate_plot(index, dependent):
+	values = pd.unique(df[index])
+	retrieve = lambda value: df.loc[df[index] == value]
 
-plt.boxplot(acc, 1)
-plt.plot(acc)
+	filtered = [retrieve(value) for value in values]
+	y = [frame[dependent] for frame in filtered]
+
+	for vals, lr in zip(y, values):
+		print(lr)
+		print(vals)
+		print(np.median(vals))
+
+	print(values)
+	plt.boxplot(y)
+	plt.show()
+	#acc = filtered[]
+
+
+generate_plot('learning_rate', 'best_validation_loss')
+
+#plt.boxplot(acc)
+#plt.show()
