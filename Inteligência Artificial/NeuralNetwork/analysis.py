7e4f8e2f8c5470aee1fc7ae9d6806727aaadff54
Fernando Concatto <fernandoconcatto@gmail.com>
2018-05-29 08:45:32 -0300

    Leves melhoramentos na geração de gráficos

--- Inteligência Artificial/NeuralNetwork/analysis.py
+++ Inteligência Artificial/NeuralNetwork/analysis.py
@@ -3,15 +3,25 @@ import pandas as pd
 import numpy as np
 
 path = "/home/concatto/Downloads/GoodResultsNN/4/experiments_portion0.csv"
-df = pd.read_csv(path)
-df = df.loc[df['hidden_activation'] == 'sigmoid']
-df = df.loc[df['output_activation'] == 'sigmoid']
+df_full = pd.read_csv(path)
 
-def generate_plot(index, dependent):
+def select(frame, variable, value):
+	return frame.loc[frame[variable] == value]
+
+def generate_plot(index, dependent, xlabel=None, ylabel=None, title="Relacionamento parâmetro x qualidade"):
+	df = df_full
+	df = select(df, 'hidden_activation', 'sigmoid')
+	df = select(df, 'output_activation', 'sigmoid')
+
+
+	if xlabel == None:
+		xlabel = index
+	if ylabel == None:
+		ylabel = dependent
+
 	values = pd.unique(df[index])
-	retrieve = lambda value: df.loc[df[index] == value]
 
-	filtered = [retrieve(value) for value in values]
+	filtered = [select(df, index, value) for value in values]
 	y = [frame[dependent] for frame in filtered]
 
 	for vals, lr in zip(y, values):
@@ -20,12 +30,15 @@ def generate_plot(index, dependent):
 		print(np.median(vals))
 
 	print(values)
-	plt.boxplot(y)
+	plt.boxplot(y, labels=values)
+	plt.ylabel(ylabel)
+	plt.xlabel(xlabel)
+	plt.title(title)
 	plt.show()
 	#acc = filtered[]
 
 
-generate_plot('learning_rate', 'best_validation_loss')
+generate_plot('learning_rate', 'best_validation_loss', xlabel="Taxa de Aprendizagem", ylabel="Erro Quadrático Médio")
 
 #plt.boxplot(acc)
 #plt.show()
